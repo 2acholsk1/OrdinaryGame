274bb433cb4e3e1b2032763a2d62473f68b2af62
2acholsk1 <p.zacholski@wp.pl>
2021-05-22 17:26:44 +0200

    Customtextures dont crush

--- customtexture.cpp
+++ customtexture.cpp
@@ -4,3 +4,37 @@ CustomTexture::CustomTexture()
 {
 
 }
+
+CustomTexture::~CustomTexture()
+{
+
+}
+
+void CustomTexture::AddTexture(const MyTexture& textureKey,const std::string& filename)
+{
+    if(this->textures.find(textureKey)!=this->textures.end())
+    {
+        throw std::invalid_argument("Element already existed");
+    }
+
+    sf::Texture UnosTexturos;
+
+    if(!UnosTexturos.loadFromFile(filename))
+    {
+        throw std::invalid_argument("File no open");
+    }
+
+    this->textures.insert(std::pair<MyTexture,sf::Texture>(textureKey,UnosTexturos));
+}
+
+sf::Texture& CustomTexture::GetTexture(const MyTexture& textureKey)
+{
+    if(this->textures.find(textureKey)==this->textures.end())
+    {
+        throw std::out_of_range("Can't find it");
+    }
+
+    return this->textures.at(textureKey);
+}
+
+
--- customtexture.h
+++ customtexture.h
@@ -1,11 +1,31 @@
 #ifndef CUSTOMTEXTURE_H
 #define CUSTOMTEXTURE_H
 
+#include <map>
+
+#include <SFML/Graphics.hpp>
+
+enum class MyTexture
+{
+    Player,
+    Mob,
+    Item,
+    InterfacePart,
+    Tree,
+    Structure,
+    Minerals
+};
+
 
 class CustomTexture
 {
+private:
+    std::map<MyTexture,sf::Texture> textures;
 public:
     CustomTexture();
+    ~CustomTexture();
+    void AddTexture(const MyTexture& textureKey,const std::string& filename);
+    sf::Texture& GetTexture(const MyTexture& textureKey);
 };
 
 #endif // CUSTOMTEXTURE_H
